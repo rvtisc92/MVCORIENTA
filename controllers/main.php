fadd22ada80ce7326ad336dd057b5a3eaf403138
rvtisc92 <30668209+rvtisc92@users.noreply.github.com>
2021-08-07 12:02:34 -0500

    Configuración inicial.
    Configuración del htaccess.
    Crear controladores.

---
+++ controllers/main.php
@@ -0,0 +1,14 @@
+<?php
+  class main
+  {
+    function __construct()
+    {
+      echo "<p>Nuevo controlador Main</p>";
+    }
+
+    function saludo()
+    {
+      echo "<p>Ejecutaste el metodo saludo</p>";
+    }
+  }
+?>
---
+++ libs/app.php
@@ -0,0 +1,33 @@
+<?php
+  require_once 'controllers/errormsg.php';
+  class App
+  {
+    function __construct()
+    {
+      echo "<p>Nueva app</p>";
+
+      $url = $_GET['url'];
+      $url = rtrim($url, '/');
+      $url = explode('/', $url);
+
+      // var_dump($url);
+
+      $archivoController = 'controllers/'.$url[0].'.php';
+
+      if(file_exists($archivoController))
+      {
+        require_once $archivoController;
+        $controller = new $url[0];
+
+        if(isset($url[1]))
+        {
+          $controller->{$url[1]}();
+        }
+      }
+      else
+      {
+        $controller = new errormsg();
+      }
+    }
+  }
+?>
